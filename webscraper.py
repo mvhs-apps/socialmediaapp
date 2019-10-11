694a1c724740535971253e728d6de0aeaae4abfa
Archish Arun <archisharun99@gmail.com>
2019-10-10 23:09:58 -0700

    create webscraper.py
    simple webscraper with functionality on politico.com, to be expanded to other sites and outlets.
    
    sends json data to text file lmfao

---
+++ webscraper.py
@@ -0,0 +1,35 @@
+import requests
+import urllib.request
+import time
+import bs4
+import json
+
+
+
+url1 = "https://politico.com"
+
+
+politicoRes = requests.get(url1)
+soup = bs4.BeautifulSoup(politicoRes.text, "html.parser")
+
+
+links = {}
+links['politico'] = []
+
+
+for ana in soup.findAll('a'):
+    if ana.parent.name == 'h1':
+        download_url = ana['href']
+        print(ana['href'])
+        links['politico'].append({
+        'link': download_url,
+        'head': ana.text
+        })
+    time.sleep(1)
+    
+
+ 
+
+
+with open('data.txt', 'w') as outfile:
+    json.dump(links, outfile)
