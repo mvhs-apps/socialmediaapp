7446844b6c6f72201c96ac9aaeed66496954c2ea
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-11-01 22:39:41 -0700

    Make grid responsive

--- explore.php
+++ explore.php
@@ -6,45 +6,53 @@ $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 session_start();
 $user = stripper($_SESSION["user"]);
 if($user == "invalid") {
-    header("Location: http://localhost/socialmediaapp/index.php");
+    header("Location: index.php");
 }
 ?>
 
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+    <link href="explore.css" rel="stylesheet">
+</head>
 <body>
 
+<div class='px-2 card-grid'>
 <?php
 //$command = escapeshellcmd('python webscraper.py');
 //shell_exec($command);
 $jsonn = readData('data.txt');
-$politico = $jsonn["politico"];
-for($i = 1; $i < sizeof($politico); $i++) {
-    $link = $politico[$i]["link"];
-    echo "<!-- Card -->
-    <div class=\"card\">
+foreach($jsonn as $site => $list){
+    echo "<h2 class='m-2 my-3 full-width'>" . ucwords($site) . "</h2>";
+    foreach($list as $item){
+        $link = $item["link"];
+        echo "<!-- Card -->
+        <div class='card'>
 
-    <!-- Card image -->
-    <div class=\"view overlay\">
-        <img class=\"card-img-top\" src=\"https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg\" alt=\"Card image cap\">
-        <a href='$link'>
-    <div class=\"mask rgba-white-slight\"></div>
-        </a>
-    </div>
+            <!-- Card image -->
+            <div class='view overlay'>
+                <img class='card-img-top' src='https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg' alt='Card image cap'>
+                <a href='$link'>
+            <div class='mask rgba-white-slight'></div>
+                </a>
+            </div>
 
-    <!-- Card content -->
-    <div class=\"card-body\">
+            <!-- Card content -->
+            <div class='card-body'>
 
-    <!-- Title -->
-    <a href='$link'><h4 class=\"card-title\">";
-    echo $politico[$i]["head"];
-    echo "</a></h4></div>
+            <!-- Title -->
+            <a href='$link'><h4 class='card-title'>
+            {$item["head"]}
+            </a></h4></div>
 
-</div>
-<!-- Card -->";
+        </div>
+        <!-- Card -->";
+    }
 }
 ?>
+
+</div>
+
 <body>
 
 </html>
